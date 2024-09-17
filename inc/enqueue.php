067a36dd228d53610afef17a499380db9d80b2df
benbegins <62623777+benbegins@users.noreply.github.com>
2024-09-17 10:06:31 +0200

    speed optimization

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -7,7 +7,7 @@ if ( ! function_exists( 'bemy_scripts' ) ) {
 
 		// Style
 		wp_enqueue_style( 'fonts', 'https://use.typekit.net/ggh8bso.css', array(), null, 'all' );
-		wp_enqueue_style( 'bemy-css', get_template_directory_uri() . '/dist/style.css', array('fonts'), wp_get_theme()->get( 'Version' ) );
+		wp_enqueue_style( 'bemy-css', get_template_directory_uri() . '/dist/style.css', array(), wp_get_theme()->get( 'Version' ) );
 		wp_enqueue_script( 'axios', "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js", array(), false, true );
 		wp_enqueue_script( 'bemy-script', get_template_directory_uri() . '/dist/init.js', array('axios'), wp_get_theme()->get( 'Version' ), true );
 		if ( is_page_template( 'template-pages/contact.php' ) ) {
